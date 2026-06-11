228b9efec0ac1cab572022638a5feb7176abe0dd
alanc-ux <alan_c@dscsolutionsfm.com>
2026-06-11 12:15:45 -0400

    Update hubspot-accounts.js

--- api/hubspot-accounts.js
+++ api/hubspot-accounts.js
@@ -169,7 +169,7 @@ async function fetchWorkers(accountHsIds) {
   return enriched.filter(Boolean);
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
